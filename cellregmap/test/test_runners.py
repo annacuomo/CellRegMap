3cd12c4d0e198928dc7b1541979f219a46951109
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-22 14:37:00 +1000

    add more runner tests

--- cellregmap/test/test_runners.py
+++ cellregmap/test/test_runners.py
@@ -2,20 +2,23 @@ import unittest
 
 from numpy import ones
 from numpy.random import RandomState
+from numpy_sugar import ddot
 from numpy_sugar.linalg import economic_svd
 
 from cellregmap import CellRegMap
 from cellregmap import run_interaction, run_association, run_association_fast
 
-random = RandomState(0)
-n = 30                               # number of samples (cells)
-p = 5                                # number of individuals
-k = 4                                # number of contexts
+random = RandomState(10)
+n = 100                               # number of samples (cells)
+p = 10                                # number of individuals
+k = 5                                # number of contexts
 y = random.randn(n, 1)               # outcome vector (expression phenotype)
 C = random.randn(n, k)               # context matrix  
 W = ones((n, 1))                     # intercept (covariate matrix)
 hK = random.rand(n, p)               # decomposition of kinship matrix (K = hK @ hK.T)
-g = 1.0 * (random.rand(n, 2) < 0.2)  # SNP vector
+g = 1.0 * (random.rand(n, 1) < 0.2)  # SNP vector
+
+delta = 0.02
 
 class TestRunners(unittest.TestCase):
 
@@ -41,9 +44,18 @@ class TestRunners(unittest.TestCase):
     def test_fast_association_runner(self):
         #### old approach
         crm = CellRegMap(y, W, C, hK=hK) # fit null model (hK -> background is K + EEt)
-        pv_old = crm.scan_association(g)[0]
+        pv_old = crm.scan_association_fast(g)[0]
         #### new approach
-        pv_new = run_association_fast(y, W, C, g, hK=hK)[0]
-        self.assertEqual(pv_old, pv_new) # change this to allow tolerance
+        pv_new = run_association_fast(y, W, C, g, hK=hK)[0] # faster but less accurate method
+        message = "first and second are not almost equal."
+        self.assertEqual(pv_old, pv_new)
+
+    def test_slow_fast_association_runner(self, delta=delta):
+        #### regular approach
+        pv_slow = run_association(y, W, C, g, hK=hK)[0] 
+        #### fast approach
+        pv_fast = run_association_fast(y, W, C, g, hK=hK)[0] # faster but less accurate 
+        message = "first and second are not almost equal."
+        self.assertAlmostEqual(pv_slow, pv_fast, None, message, delta) # allow tolerance
 
 
